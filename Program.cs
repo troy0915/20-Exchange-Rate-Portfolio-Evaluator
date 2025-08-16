8047ecd9e73d36d0537e8ccbbee4bf2b72e2b758
troy0915 <johntroydolom7@gmail.com>
2025-08-16 18:18:00 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Asset
+{
+    public string Currency { get; }
+    public decimal Amount { get; }
+
+    public Asset(string currency, decimal amount)
+    {
+        Currency = currency.ToUpper();
+        Amount = amount;
+    }
+
+    public decimal ValueInHomeCurrency(Dictionary<string, decimal> latestRates)
+    {
+        if (!latestRates.ContainsKey(Currency))
+            throw new Exception($"No valid FX rate for currency {Currency}");
+        return Amount * latestRates[Currency];
+    }
+
+    public override string ToString()
+    {
+        return $"{Amount} {Currency}";
+    }
+}
+
+public class FxRate
+{
+    public string Currency { get; }
+    public DateTime Date { get; }
+    public decimal RateToHome { get; }
+
+    public FxRate(string currency, DateTime date, decimal rate)
+    {
+        Currency = currency.ToUpper();
+        Date = date;
+        RateToHome = rate;
+    }
+}
+
+
+namespace _20__Exchange_Rate_Portfolio_Evaluator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var assets = new List<Asset>
+        {
+            new Asset("USD", 1000),
+            new Asset("EUR", 500),
+            new Asset("JPY", 100000),
+            new Asset("GBP", 200)
+        };
+
+           
+            var fxRates = new List<FxRate>
+        {
+            new FxRate("USD", new DateTime(2025, 8, 10), 56.20m),
+            new FxRate("USD", new DateTime(2025, 8, 14), 56.50m), 
+            new FxRate("EUR", new DateTime(2025, 8, 14), 61.25m),
+            new FxRate("JPY", new DateTime(2025, 8, 12), 0.39m),
+            new FxRate("GBP", new DateTime(2025, 7, 20), 71.10m) 
+        };
+
+            DateTime valuationDate = new DateTime(2025, 8, 14);
+            int staleDaysLimit = 7; 
+
+            var latestRates = fxRates
+                .Where(r => (valuationDate - r.Date).TotalDays <= staleDaysLimit)
+                .GroupBy(r => r.Currency)
+                .ToDictionary(
+                    g => g.Key,
+                    g => g.OrderByDescending(r => r.Date).First().RateToHome
+                );
+
+            Console.WriteLine("Latest Valid FX Rates:");
+            foreach (var kvp in latestRates)
+                Console.WriteLine($"{kvp.Key} -> {kvp.Value}");
+
+            var valuedAssets = assets
+                .Where(a => latestRates.ContainsKey(a.Currency))
+                .Select(a => new
+                {
+                    Asset = a,
+                    Value = a.ValueInHomeCurrency(latestRates)
+                })
+                .OrderByDescending(x => x.Value)
+                .ToList();
+
+            Console.WriteLine("\nAssets Sorted by Value:");
+            foreach (var va in valuedAssets)
+                Console.WriteLine($"{va.Asset} = {va.Value:N2} Home Currency");
+
+            decimal totalValue = valuedAssets.Sum(a => a.Value);
+            Console.WriteLine($"\nTotal Portfolio Value: {totalValue:N2} Home Currency");
+
+            var rejected = assets
+                .Where(a => !latestRates.ContainsKey(a.Currency))
+                .ToList();
+
+            if (rejected.Any())
+            {
+                Console.WriteLine("\nRejected Assets (Stale/Missing Rates):");
+                foreach (var a in rejected)
+                    Console.WriteLine(a);
+            }
+        }
+    }
+}
+
+
+
+
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("20) Exchange Rate Portfolio Evaluator")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("20) Exchange Rate Portfolio Evaluator")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("5ef25725-ae01-4ede-b7f9-c833f5e9fc27")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
